32f5c4a3e12bfcdf6b1a41496e5de6b2b2b4e342
User-3396 <alveslucio02@outlook.com>
2026-05-28 18:50:25 +0000

    update

--- Code_in_Blocks/Blocks/classes.js
+++ Code_in_Blocks/Blocks/classes.js
@@ -1,3 +1,47 @@
+Mouse ={
+    //position: {x: 0, y: 0},
+    click: false,
+    nodeDrag: null,
+    nodeAnchor: {x: 0, y: 0}, 
+    clickPosition: {x: 0, y: 0},
+};
+
+window.onmousemove=(e)=>{
+    //Mouse.position ={x: e.clientX, y: e.clientY};
+    
+    if (Mouse.nodeDrag){
+        let target =Mouse.nodeDrag;
+        //console.log(Mouse.nodeDrag.style.left +": "+e.clientX +", "+e.offsetX);
+        Mouse.nodeDrag.style.left =(e.clientX -Mouse.nodeAnchor.x)+'px';
+        Mouse.nodeDrag.style.top =(e.clientY -Mouse.nodeAnchor.y)+'px';
+        Log.SetText ("[Node:" +Mouse.nodeDrag.name +"] px: " +e.clientX +", py: " +e.clientY);
+    }
+}
+
+window.onmousedown =(e)=>{
+    Mouse.click =true;
+    //console.log(e);
+    //if (e.target.tagName == 'NAV') 
+    //NodeList[0].setPos(30, 100, e.x, e.y);
+
+}
+
+window.onmouseup =(e)=>{
+    Mouse.click =false; 
+    Mouse.nodeDrag =null;
+    //
+}
+
+window.onmouseleave =()=>{Mouse.click =false; Mouse.nodeDrag =null;}
+
+Log ={
+    element: document.getElementById("log"), 
+    SetText: (txt)=>{
+        console.log(this.element);
+    }
+}
+
+
 class Node {
     constructor (name, x, y){
         this.name =name;
@@ -7,17 +51,28 @@ class Node {
         this.cnv =document.createElement("canvas");
         this.cnv.id ="canvas-webgl";
         this.element =document.createElement("div");
-        this.element.id ="node";
+        this.element.className ="draggable";
+        this.element.style.left =x+"px";
+        this.element.style.top =y+"px";
         this.element.appendChild(this.cnv);
         
         document.getElementById("editor").appendChild(this.element);
 
-        this.element.onmousedown=()=>{
+        //this.element.addEventListener('mousedown',(e)=>{
+        this.element.onmousedown=(e)=>{
+            Mouse.nodeDrag =this.element;
+            Mouse.nodeAnchor ={x: e.offsetX, y: e.offsetY};
+
+            //header.innerText =e.offsetX+'px';
+            //console.log(Mouse);
+        };
+
+        /*this.element.onmousedown=(e)=>{
             this.drag =true;
             // Calcula a distância entre onde o mouse clicou e os cantos da div
             this.mouseOffset = {
-                x: e.clientX - this.element.getBoundingClientRect().left, 
-                y: e.clientY - this.element.getBoundingClientRect().top
+                x: this.element.offsetLeft -e.clientX , 
+                y: this.element.offsetTop -e.clientX
             };
         }
         this.element.onmouseup=()=>{
@@ -29,10 +84,12 @@ class Node {
         this.element.onmousemove=(e)=>{
             if (this.drag){
                 // Atualiza a posição da div baseada nas coordenadas do mouse
-                this.element.style.left =(e.clientX -this.mouseOffset.x) +'px';
-                this.element.style.top =(e.clientY -this.mouseOffset.y) +'px';
+                this.element.style.left =(e.clientX +this.mouseOffset.x) +'px';
+                this.element.style.top =(e.clientY +this.mouseOffset.y) +'px';
             }
-        }
+
+            document.querySelectorAll("p.log")[0].textContent ="client: " +e.clientX +", " +e.clientY +"; offset: " +e.offsetX +", " +e.offsetY +";";
+        }*/
 
 
     }
@@ -41,5 +98,5 @@ class Node {
 
     }
 
-
 }
+
